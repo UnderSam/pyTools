22fe636c0e52841abb26db50eb406f6743da4e4f
HandsomeSam <sam19970416@gmail.com>
2018-10-16 16:33:45 +0800

    Fix : Wrong if else sort

--- getWorkday.py
+++ getWorkday.py
@@ -3,16 +3,16 @@ from datetime import date,timedelta
 import datetime
 import sys
 def getWorkDay(delt=None):
-    if str(delt).isdigit():
-        if delt == None:
-            workday = datetime.date.today()
-            if workday.weekday()==0:
-                workday = workday - timedelta(3)
-            else:
-                workday = workday - timedelta(1)
-            return workday
+    if delt == None:
+        workday = datetime.date.today()
+        if workday.weekday()==0:
+            workday = workday - timedelta(3)
         else:
+            workday = workday - timedelta(1)
+        return workday
+    else:
+        if str(delt).isdigit():
             workday = datetime.date.today()
             return workday - timedelta(int(delt))
-    else:
-        sys.exit("Please imput number (timedelta)")
+        else:
+            sys.exit("Please imput number (timedelta)")
