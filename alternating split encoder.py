aa2d9be25d35bff87d7dbaebf2528267ef8d8b14
HandsomeSam <sam19970416@gmail.com>
2018-11-20 17:34:56 +0800

    Bug : temp fix with none type,but odd num bug stil

--- alternating split encoder.py
+++ alternating split encoder.py
@@ -1,13 +1,19 @@
 def encrypt(text, n):
+    if text is None:
+        return None
     if len(text) == 0 or n <=0 :
         return text
+
     for __ in range(n):
         text = text[1::2]+text[0::2]
     return text
 
 def decrypt(encrypted_text, n):
+    if encrypted_text is None:
+        return None
     if len(encrypted_text) == 0 or n <=0 :
         return encrypted_text
+
     new = ""
     mid = len(encrypted_text)//2
     for __ in range(n):
