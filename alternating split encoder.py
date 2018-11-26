1ac002fe1b10656f583b8162b6cbd82b68966968
HandsomeSam <sam19970416@gmail.com>
2018-11-26 09:13:13 +0800

    Finish : Fix the last bug.

--- alternating split encoder.py
+++ alternating split encoder.py
@@ -3,7 +3,6 @@ def encrypt(text, n):
         return None
     if len(text) == 0 or n <=0 :
         return text
-
     for __ in range(n):
         text = text[1::2]+text[0::2]
     return text
@@ -13,13 +12,14 @@ def decrypt(encrypted_text, n):
         return None
     if len(encrypted_text) == 0 or n <=0 :
         return encrypted_text
-
     new = ""
     mid = len(encrypted_text)//2
     for __ in range(n):
         temp = ""
         for i in range(mid):
             temp += encrypted_text[mid:][i]+encrypted_text[:mid][i]
+        if len(encrypted_text) %2 !=0:
+            temp += encrypted_text[-1]
         encrypted_text = temp
         new = temp
     return new
